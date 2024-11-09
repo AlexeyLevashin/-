e2c67dd20f254c1520a3a9b11bc3383f1bc7b0ef
levalex <levalex814@gmail.com>
2024-11-09 10:06:54 +0300

    Encapsulation

---
+++ Encapsulation/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class BankAccount
+    {
+        private int Balance;
+
+        public int GetBalanseValue()
+        {
+            return Balance; 
+        }
+        public void SetBalanseValue(int balance)
+        {
+            Balance = balance;            
+        }
+
+        public void AddMoney(int value)
+        {
+            if(value > 0)
+            {
+                Balance += value;
+            }
+            else
+                Console.WriteLine("Сумма пополнения должна быть положительной");
+        }
+
+        public void GetMoney(int value) {
+            if ((value > 0) && (Balance - value >= 0))
+            {
+                Balance -= value;
+            }
+            else if (value <= 0)
+                Console.WriteLine("Сумма списания должна быть положительной");
+            else
+                Console.WriteLine("На счете недостаточно средств");
+
+         }
+         
+        public void CorrectInput(string input, ref int output)
+        {
+            while (true)
+            {
+                if (double.TryParse(input, out var number))
+                {
+                    output = Convert.ToInt32(Math.Floor(number));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Вам нужно ввести значение в числовом формате, например: {"107"} ");
+                    Console.Write("Введите значение: ");
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount();
+            Console.WriteLine("Приветсвуем вас в приложении нашего банка.\n" +
+                "Сейчас вам будет предложено ввести исходный баланс.\n" +
+                "При вводе не целого числа копейки будут списаны банком в качестве комиссии.");
+            Console.Write("Введите ваш исходный баланс: ");
+
+            int value=0;
+            bankAccount.CorrectInput(Console.ReadLine(), ref value);
+            bankAccount.SetBalanseValue(value);
+            Console.WriteLine($"Ваш баланс: {bankAccount.GetBalanseValue()}");
+
+            Console.WriteLine("Выберите, какую операцию хотите произвести ");
+            Console.WriteLine($"Введите 1, если хотитите пополнить счет");
+            Console.WriteLine($"Введите 2, если хотитите снять деньги со счета");
+            Console.WriteLine($"Введите 3, если хотитите покинуть приложение банка");
+
+            while (true)
+            {
+                Console.Write("Введите номер операции: ");
+                bankAccount.CorrectInput(Console.ReadLine(), ref value);
+                if (value == 1)
+                {
+                    Console.WriteLine("Введите сумму пополнения: ");
+                    bankAccount.CorrectInput(Console.ReadLine(), ref value);
+                    bankAccount.AddMoney(value);
+                }
+                else if(value == 2)
+                {
+                    Console.WriteLine("Введите сумму, которую хотите снять: ");
+                    bankAccount.CorrectInput(Console.ReadLine(), ref value);
+                    bankAccount.GetMoney(value);
+                }
+                else if(value == 3)
+                {
+                    break;                  
+                }
+                Console.WriteLine($"Ваш текущий баланс: {bankAccount.GetBalanseValue()}");
+            }
+
+            Console.WriteLine($"Нажмите  {"Enter"}");
+            Console.ReadLine();
+            
+        }
+    }
+}
