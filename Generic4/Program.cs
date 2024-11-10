bc1687ac77075a8822c8283e1a1e4c0d29369d43
levalex <levalex814@gmail.com>
2024-11-10 13:44:49 +0300

    Generic4

---
+++ Generic4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generic4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=7, b=6;
+            Console.WriteLine($"До обмена: a = {a}, b = {b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"После обмена: a = {a}, b = {b}");
+
+            Console.ReadLine();
+        }
+        public static void Swap<T>(ref T first, ref T second) 
+        {
+            T copy = first;
+            first = second;
+            second = copy;
+        }
+    }
+}
