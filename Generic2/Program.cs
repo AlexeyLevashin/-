8a40837884bdab3b083919fe22c0ddcf35e81111
levalex <levalex814@gmail.com>
2024-11-10 12:37:22 +0300

    Generic2

---
+++ Generic2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 11, 2, 45, 20, -8, 9 };
+            Console.WriteLine($"Минимальное: { FindMin(numbers)}");
+
+            string[] words = {"apple", "banana", "books", "pen" };
+            Console.WriteLine($"Минимальное: {FindMin(words)}");
+
+            Console.ReadLine();
+        }
+        public static T FindMin<T>(T[] mas) where T: IComparable<T>
+        {
+            T min = mas[0];
+            foreach (T t in mas)
+            {
+                if(t.CompareTo(min)<0)
+                    min = t;
+            }
+            return min;
+        } 
+    }
+}
