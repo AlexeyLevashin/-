93126643bda7b64aac3589c880a4af326dab0b17
levalex <levalex814@gmail.com>
2024-11-10 14:27:35 +0300

    Добавил try catch

--- Generic3/Program.cs
+++ Generic3/Program.cs
@@ -7,10 +7,21 @@ namespace Generic3
         static void Main(string[] args)
         {
             Repository<Car> repository = new Repository<Car>();
-            repository.Add(new Car { Brand = "Mers", Model = "Cls90" });
-            repository.Add(new Car { Brand = "BMW", Model = "5" });
-            var car = repository.Get(1);
-            Console.WriteLine($"Car: {car.Brand} {car.Model}");
+            try
+            {
+                repository.Add(new Car { Brand = "Mers", Model = "Cls90" });
+                repository.Add(new Car { Brand = "BMW", Model = "5" });
+                var car = repository.Get(1);
+                Console.WriteLine($"Car: {car.Brand} {car.Model}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
         }
