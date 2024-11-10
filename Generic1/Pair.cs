55f124c1f97685f7423491c996106c9b7e7169fc
levalex <levalex814@gmail.com>
2024-11-10 03:04:22 +0300

    Generic1

---
+++ Generic1/Pair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic1
+{
+    internal class Pair<T, U>
+    {
+        public T Value1 { get; set; }
+        public U Value2 { get; set; }
+
+        public Pair(T value1, U value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+    }
+}
---
+++ Generic1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Generic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, string> pair = new Pair<int, string>(3, "часа ночи");
+            Console.WriteLine($"{pair.Value1}, {pair.Value2}");
+            Console.ReadLine();
+        }
+    }
+}
